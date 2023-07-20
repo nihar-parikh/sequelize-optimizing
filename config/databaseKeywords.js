4d7d27bcf4cb55793ec4d6275ba6035525ddd2fd
Nihar Parikh <nihar.parikh@celtic.bz>
2023-07-20 18:00:43 +0530

    added_migrationsConfig_and_made_migration_dynamic_using_script_in_package.json

--- config/databaseKeywords.js
+++ config/databaseKeywords.js
@@ -6,3 +6,4 @@ exports.DATABASE_HOST = process.env.DB_HOST;
 exports.DATABASE_USERNAME = process.env.DB_USERNAME;
 exports.DATABASE_PASSWORD = process.env.DB_PASSWORD;
 exports.DATABASE_DIALECT = process.env.DB_DIALECT;
+exports.DATABASE_MIGRATION_TABLE_NAME = process.env.DB_MIGRATION_TABLE_NAME;
---
+++ config/migrationsConfig.js
@@ -0,0 +1,27 @@
+const {
+  DATABASE_NAME,
+  DATABASE_HOST,
+  DATABASE_USERNAME,
+  DATABASE_PASSWORD,
+  DATABASE_DIALECT,
+  DATABASE_MIGRATION_TABLE_NAME,
+} = require("./databaseKeywords");
+
+module.exports = {
+  production: {
+    dialect: DATABASE_DIALECT,
+    host: DATABASE_HOST,
+    database: DATABASE_NAME,
+    username: DATABASE_USERNAME,
+    password: DATABASE_PASSWORD,
+    migrationStorageTableName: DATABASE_MIGRATION_TABLE_NAME,
+  },
+  development: {
+    dialect: DATABASE_DIALECT,
+    host: DATABASE_HOST,
+    database: DATABASE_NAME,
+    username: DATABASE_USERNAME,
+    password: DATABASE_PASSWORD,
+    migrationStorageTableName: DATABASE_MIGRATION_TABLE_NAME,
+  },
+};
