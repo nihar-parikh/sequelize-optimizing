160181a971d9888c49e706d1aac72d58c2caaaea
nihar-parikh <niharparikh91@gmail.com>
2023-07-24 16:31:51 +0530

    added_package.json_and_set_env_dynamic

--- config/databaseKeywords.js
+++ config/databaseKeywords.js
@@ -1,35 +1,3 @@
-// const dotenv = require("dotenv");
-// const { DataBaseError } = require("../errors");
-
-// console.log("process.env.NODE_ENV: ", process.env.NODE_ENV);
-// // Load the environment-specific .env file based on the NODE_ENV value
-// if (process.env.NODE_ENV === "development") {
-//   dotenv.config({ path: ".env.development" });
-// } else if (process.env.NODE_ENV === "qa") {
-//   dotenv.config({ path: ".env.qa" });
-// } else if (process.env.NODE_ENV === "staging") {
-//   dotenv.config({ path: ".env.staging" });
-// } else if (process.env.NODE_ENV === "production") {
-//   dotenv.config({ path: ".env.production" });
-// } else {
-//   throw new DataBaseError(
-//     "Invalid NODE_ENV value. Please set it to development, qa, staging, or production.",
-//     "DB_CONNECTION_ERROR"
-//   );
-// }
-
-// console.log(process.env.NODE_ENV);
-
-// exports.DATABASE_NAME = process.env.DB_NAME;
-// exports.DATABASE_HOST = process.env.DB_HOST;
-// exports.DATABASE_USERNAME = process.env.DB_USERNAME;
-// exports.DATABASE_PASSWORD = process.env.DB_PASSWORD;
-// exports.DATABASE_DIALECT = process.env.DB_DIALECT;
-// exports.DATABASE_MIGRATION_TABLE_NAME = process.env.DB_MIGRATION_TABLE_NAME;
-// exports.JWT_TOKEN_SECRET = process.env.JWT_TOKEN_SECRET;
-// exports.ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
-// exports.INITIALIZATION_VECTOR = process.env.INITIALIZATION_VECTOR;
-
 const dotenv = require("dotenv");
 const { DataBaseError } = require("../errors");
 
@@ -40,7 +8,7 @@ const envFilePaths = {
   staging: ".env.staging",
   production: ".env.production",
 };
-console.log("process.env.NODE_ENV>>>>>> ", process.env.NODE_ENV);
+console.log("process.env.NODE_ENV --->>>>>>", process.env.NODE_ENV);
 // Get the appropriate .env file path based on NODE_ENV or fallback to development
 const envFilePath =
   envFilePaths[process.env.NODE_ENV] || envFilePaths.development;
--- config/migrationsConfig.js
+++ config/migrationsConfig.js
@@ -6,14 +6,7 @@ const {
   DATABASE_DIALECT,
   DATABASE_MIGRATION_TABLE_NAME,
 } = require("./databaseKeywords");
-// console.log({
-//   DATABASE_NAME,
-//   DATABASE_HOST,
-//   DATABASE_USERNAME,
-//   DATABASE_PASSWORD,
-//   DATABASE_DIALECT,
-//   DATABASE_MIGRATION_TABLE_NAME,
-// });
+
 module.exports = {
   development: {
     dialect: DATABASE_DIALECT,
